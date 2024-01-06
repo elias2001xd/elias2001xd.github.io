84cfbb2e685ac9660fcdbacf4671130369f44311
elias2001xd <92000129+elias2001xd@users.noreply.github.com>
2024-01-06 16:30:25 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var miBoton = document.getElementById('mybutton')
+    miBoton.addEventListener('click', function(){
+        alert('Hola visitante, esta es una página incompleta, no puedes ver mi Facebook, sólo es una prueba por el momento:D');
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+    var miBoton2 = document.getElementById('mybutton2')
+    miBoton2.addEventListener('click', function(){
+        alert('Hola visitante, esta es una página incompleta, no puedes ver mi Instagram, sólo es una prueba por el momento:D');
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+    var miBoton3 = document.getElementById('mybutton3')
+    miBoton3.addEventListener('click', function(){
+        alert('Hola visitante, si quieres visitar mi Linkedin, pulsa el bóton que se encuentra arriba que dice "contratar YA" :D');
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+    var miBoton4 = document.getElementById('mybutton4')
+    miBoton4.addEventListener('click', function(){
+        alert('no disponible por el momento:D');
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+    var miBoton5 = document.getElementById('mybutton5')
+    miBoton5.addEventListener('click', function(){
+        alert('no disponible por el momento:D');
+    })
+})
+document.addEventListener('DOMContentLoaded', function(){
+    var miBoton6 = document.getElementById('mybutton6')
+    miBoton6.addEventListener('click', function(){
+        alert('no disponible por el momento:D');
+    })
+})
